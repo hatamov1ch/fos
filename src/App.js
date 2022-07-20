fce899f16574ce6650c2a95bc9de42a3204d1b08
hatamov1ch <tursunaliev.codes@proton.me>
2022-07-20 07:25:26 +0500

    prepared global styles and added google fonts api

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import Layout from "./components/Layout";
+
+const App = () => <Layout></Layout>;
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import App from "./App";
+import GlobalStyle from "./global.styles.js";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("root");
+
+const root = createRoot(rootElement);
+
+// Rendering main component as well as global styles...
+root.render(
+	<StrictMode>
+		<App />
+		<GlobalStyle />
+	</StrictMode>
+);
